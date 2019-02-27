bd744a9fada0446bc78970ac906543c892296cc6
ruritoblogger <rurito.blog@gmail.com>
2019-02-28 00:50:16 +0900

    仮完成

--- python.py
+++ python.py
@@ -1,21 +1,36 @@
 import time
 
-def get_time():
+def get_mode():
+    ans = input("一分以上の時間を計測しますか?(y or n)")
+    if ans == "y":
+        return True
+    elif ans == "n":
+        return False
+
+def get_time(mode):
     while True:
-        n = input("設定したい時間を入力してください:")
+        if mode:
+            n = input("設定したい時間を入力してください(分):")
+        else:
+            n = input("設定したい時間を入力してください(秒):")
         if n.isdigit():
             break
         else:
-            print("時間を入力してください(秒)\n")
+            print("時間を入力してください\n")
     return n
 
 def main():
     print("タイマーを起動しました")
-
+    
+    # 1分以上かどうか
+    mode = get_mode()
     # 計測時間の受け取り
-    target_time = get_time()
+    target_time = float(get_time(mode))
     #計測開始時間の取得
     start_time = time.time()
+
+    if mode:
+        target_time *= 60
     tmp = int(float(target_time) - time.time() + start_time)
     tmp2 = tmp + 1
     while True:
@@ -25,10 +40,17 @@ def main():
         elif tmp != int(float(target_time) - time.time() + start_time) - 0.2 or tmp != int(float(target_time) - time.time() + start_time) + 0.2:
             if tmp != tmp2:
                 tmp2 = tmp
-                if(tmp + 1 > 9 ):
-                    print(tmp + 1)
+                if mode:
+                    #if (tmp + 1) / 60 != 0:
+                    if (tmp + 1) % 60 > 9:
+                        print(str(int((tmp + 1) / 60)) + "分" + str((tmp + 1) % 60) + "秒")
+                    else:
+                        print(str(int((tmp + 1) / 60)) + "分0" + str((tmp + 1) % 60) + "秒")
                 else:
-                    print("0" + str(tmp + 1))
+                    if tmp + 1 > 9:
+                        print(tmp + 1)
+                    else:
+                        print("0" + str(tmp + 1))
         tmp = int(float(target_time) - time.time() + start_time)
 
 if __name__ == '__main__':
