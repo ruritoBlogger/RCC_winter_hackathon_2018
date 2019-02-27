de2dd7aa93f432c46b5872db3d1d968d1cd6d534
ruritoblogger <rurito.blog@gmail.com>
2019-02-28 00:26:42 +0900

    カウントダウン実装

--- python.py
+++ python.py
@@ -1,5 +1,4 @@
 import time
-import tkinter as tk
 
 def get_time():
     while True:
@@ -10,36 +9,24 @@ def get_time():
             print("数字を入力してください\n")
     return n
 
-def set_gui():
-    root = tk.Tk()
-    root.title("キッチンタイマー")
-    root.geometry("640x480")
-    root["relief"] = 'ridge'
-    root["borderwidth"] = 10
-
-
-    # ウインドウに表示するものの設定
-    label = tk.Label(root, text="This is kittin timer")
-    label["background"] = '#ffffff'
-    label["width"] = 240
-    label.grid()
-    
-    return root
-
 def main():
-    # GUIの表示
-    root = set_gui()
     print("タイマーを起動しました")
 
     # 計測時間の受け取り
     target_time = get_time()
     #計測開始時間の取得
     start_time = time.time()
-
+    tmp = int(float(target_time) - time.time() + start_time)
+    tmp2 = tmp + 1
     while True:
         if time.time() - start_time > int(target_time):
             print("設定した時間が経過しました")
             break
+        elif tmp != int(float(target_time) - time.time() + start_time) - 0.2 or tmp != int(float(target_time) - time.time() + start_time) + 0.2:
+            if tmp != tmp2:
+                tmp2 = tmp
+                print(tmp + 1)
+        tmp = int(float(target_time) - time.time() + start_time)
 
 if __name__ == '__main__':
     main()
