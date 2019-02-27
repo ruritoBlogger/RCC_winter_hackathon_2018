14578574968a062888a3e6de972384432878e7ea
ruritoblogger <rurito.blog@gmail.com>
2019-02-27 22:25:28 +0900

    add comment

--- python.py
+++ python.py
@@ -11,10 +11,18 @@ def getTime():
     return n
 
 def main():
+    # GUIの表示
+    root = tk.Tk()
+    root.title("キッチンタイマー")
+    root.geometry("640x480")
     print("タイマーを起動しました")
+
+    # 計測時間の受け取り
     target_time = getTime()
+    #計測開始時間の取得
     start_time = time.time()
-        while True:
+
+    while True:
         if time.time() - start_time > int(target_time):
             print("設定した時間が経過しました")
             break
