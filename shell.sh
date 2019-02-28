4c6ec64631df87d817ebc73ec64069317be80e25
ruritoblogger <rurito.blog@gmail.com>
2019-02-28 12:48:03 +0900

    入力受け取り部分実装

--- shell.sh
+++ shell.sh
@@ -1,1 +1,22 @@
-echo "Hello, World!"
+#!/bin/sh
+
+echo "タイマーを起動しました"
+echo -n "一分以上の時間を計測しますか?(y or n):"
+read ans
+if [ "$ans" = y ]
+then 
+    echo "1分以上"
+elif [ "$ans" = n ]
+then
+    echo "1分以下"
+
+fi
+
+SECONDS=0
+
+### 時間測定したい処理
+sleep 3
+
+time=$SECONDS
+
+echo $time
