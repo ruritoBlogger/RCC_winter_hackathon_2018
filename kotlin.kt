752ac28c57569a65f8c76add72f32fcf8ace9540
ruritoblogger <rurito.blog@gmail.com>
2019-03-01 05:04:00 +0900

    make input time func

--- kotlin.kt
+++ kotlin.kt
@@ -1,6 +1,6 @@
 fun get_mode() : Boolean {
-    println("分単位もしくは秒単位どちらで計測しますか?(y or n)")
-    var ans = readLine()
+    print("分単位もしくは秒単位どちらで計測しますか?(y or n)")
+    val ans = readLine()
     if (ans == "y"){
         return true
     }else if (ans == "n"){
@@ -13,8 +13,23 @@ fun get_mode() : Boolean {
     }
 }
 
+fun get_time(mode : Boolean) : Int {
+    if (mode) print("設定したい時間を入力してください(分):")
+    else print("設定したい時間を入力してください(秒):")
+    
+    try{
+        val input : Int = Integer.parseInt(readLine())
+        return input
+    }
+    catch(e : java.lang.NumberFormatException){
+        println("時間を入力してください")
+        return get_time(mode)
+    }
+}
+
 fun main(args: Array<String>){
     println("タイマーを起動しました")
     val mode = get_mode()
-    println(mode)
+    val target_time = get_time(mode)
+    println(target_time)
 }
