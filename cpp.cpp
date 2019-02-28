13c439855cdb52271f460602f49ea6d9a5a1106a
ruritoblogger <rurito.blog@gmail.com>
2019-02-28 20:14:08 +0900

    get mode

--- cpp.cpp
+++ cpp.cpp
@@ -1,8 +1,36 @@
 #include<iostream>
+#include<string>
 using namespace std;
+
+bool get_mode()
+{
+    while(1)
+    {
+        string ans;
+        cout << "一分以上の時間を計測しますか?(y or n)";
+        cin >> ans;
+        if(ans == "y")
+        {
+            return true;
+        }
+        else if(ans == "n")
+        {
+            return false;
+        }
+    }
+}
 
 int main()
 {
-    cout << "hello world!" << endl;
+    cout << "タイマーを起動しました" << endl;
+    bool mode = get_mode();
+    cout << mode << endl;
+
+
+
+
+
+
+
     return 1;
 }
