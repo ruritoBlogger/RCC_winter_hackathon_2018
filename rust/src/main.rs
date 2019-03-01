d2c1b7d4d825d797ae7ab5f33cd9fcfef8cd4e53
ruritoblogger <rurito.blog@gmail.com>
2019-03-01 15:21:37 +0900

    make input func

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,18 @@
+fn get_mode() {
+    print!("分単位もしくは秒単位どちらで計測しますか?(y or n)");
+    
+    let s = {
+    let mut s = String::new(); // バッファを確保
+    std::io::stdin().read_line(&mut s).unwrap(); // 一行読む。失敗を無視
+    s.trim_right().to_owned() // 改行コードが末尾にくっついてくるので削る
+    };
+    if s == "y" {
+        true;
+    }else if s == "n" {
+        false;
+    } else { return get_mode(); }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mode = get_mode();
 }
